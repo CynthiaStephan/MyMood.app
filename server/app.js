fc44e98e81ba849ab0ca55f67bf7f69682b4aba5
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2025-01-22 15:58:37 +0100

    new base config

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
+// Importing modules
 const express = require('express');
 const passport = require('passport');
+const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3650;
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(passport.initialize()); 
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Test réussi !' });
+  });
+
+
+// Exporting the configured Express app
+module.exports = app;
