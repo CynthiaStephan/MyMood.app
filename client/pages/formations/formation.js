19c6848d14d9135547c7be44c93b0db165f1ce4c
Thibaud Provost <thibaudprovost50@gmail.com>
2025-01-22 15:38:56 +0100

    mood slider/ bouton appel/ page formation

---
+++ client/pages/formations/formation.js
@@ -0,0 +1,18 @@
+let stagiaires = [
+    {
+        Nom : 'stagiaire 1',
+        Mood : 30
+    },
+    {
+        Nom : 'stagiaire 2',
+        Mood : 50
+    },
+    {
+        Nom : 'stagiaire 3',
+        Mood : 20
+    },
+    {
+        Nom : 'stagiaire 4',
+        Mood : 10 
+    }
+]
