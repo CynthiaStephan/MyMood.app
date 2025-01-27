d5ec8a897ebf8f7d861a7f7607e3b8a363acf20b
LyliYoune <cloe.veisenbacher@institutsolacroup.com>
2025-01-27 15:44:08 +0100

    Test blacklist

--- server/src/models/blacklistModel.js
+++ server/src/models/blacklistModel.js
@@ -1,15 +1,26 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
-
-const Blacklist = sequelize.define('blacklist', {
+const Blacklist = sequelize.define(
+  'blacklist',
+  {
     id_blacklist: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-}, {
+    adminId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    traineeId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+  },
+  {
     timestamps: false,
-});
+  }
+);
 
 module.exports = Blacklist;
--- server/src/routes/blacklistRoutes.js
+++ server/src/routes/blacklistRoutes.js
@@ -3,5 +3,13 @@ const blacklist = require('../controllers/blacklistController');
 
 const router = Router();
 
+// Ajouter un étudiant à la blacklist
+router.post('/blacklist', blacklistController.addToBlacklist);
+
+// Supprimer un étudiant de la blacklist
+router.delete('/blacklist', blacklistController.removeFromBlacklist);
+
+// Récupérer la blacklist d'un admin
+router.get('/blacklist/:adminId', blacklistController.getBlacklist);
 
 module.exports = router
