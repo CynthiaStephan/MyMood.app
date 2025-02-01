999465f7e6440a651ed74b2a3b65bc463645d19c
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2025-02-01 17:55:32 +0100

    Add login/logout routes, update API documentation, and implement cookie handler

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,5 +3,7 @@ const authController = require('../controllers/authController');
 
 const router = Router();
 
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router
